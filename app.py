0b949cafe4cb58f3dd3d81853aae5fca1ebf0b05
Nuelltech <nuelltech@gmail.com>
2026-05-08 16:36:03 +0100

    Change allow_credentials to False in CORS middleware

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Agente Trading V7 - Professional Grade")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
